114904f75e437ba6d9e7563f114167eaaf13b55e
YunusGafur <127ygafur@frhsd.com>
2025-12-18 12:53:58 -0500

    rrrr

--- java.js
+++ java.js
@@ -1,9 +1,13 @@
 var button = document.getElementById('story');
 button.addEventListener("click", getwords);
-function getwords(){
+function getwords() {
     console.log(button);
-     
+
     console.log(words);
 }
 
 var words = document.querySelectorAll("input[type=text]")
+
+placeholders.forEach((placeholder, index)) 
+
+placeholder.innerHTML = words[index].value;
